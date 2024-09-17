ac73b55f082a663a3f2af7cc896e9c972dbf1610
mardsonferreira <mardsonferreira25@gmail.com>
2024-09-16 21:58:09 -0300

    [Ferreira] Finish configure design page layout

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -3,7 +3,7 @@
 import { useRef, useState } from "react";
 import NextImage from "next/image";
 import { Rnd } from "react-rnd";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { ArrowRight, Check, ChevronsUpDown } from "lucide-react";
 
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -15,7 +15,7 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { cn } from "@/lib/utils";
+import { cn, formatPrice } from "@/lib/utils";
 
 import { RadioGroup } from "@headlessui/react";
 
@@ -26,6 +26,7 @@ import {
     MATERIALS,
     FINISHES,
 } from "@/validators/option-validator";
+import { BASE_PRICE } from "@/config/products";
 
 interface DesignConfiguratorProps {
     configId: string;
@@ -207,10 +208,116 @@ const DesignConfigurator = ({
                                         </DropdownMenuContent>
                                     </DropdownMenu>
                                 </div>
+
+                                {[MATERIALS, FINISHES].map(
+                                    ({ name, options: selectableOptions }) => (
+                                        <RadioGroup
+                                            key={name}
+                                            value={options[name]}
+                                            onChange={(val) => {
+                                                setOptions((prev) => ({
+                                                    ...prev,
+                                                    [name]: val,
+                                                }));
+                                            }}
+                                        >
+                                            <Label>
+                                                {name
+                                                    .slice(0, 1)
+                                                    .toUpperCase() +
+                                                    name.slice(1)}
+                                            </Label>
+                                            <div className="mt-3 space-y-4">
+                                                {selectableOptions.map(
+                                                    (option) => (
+                                                        <RadioGroup.Option
+                                                            key={option.value}
+                                                            value={option}
+                                                            className={({
+                                                                active,
+                                                                checked,
+                                                            }) =>
+                                                                cn(
+                                                                    "relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between",
+                                                                    {
+                                                                        "border-primary":
+                                                                            active ||
+                                                                            checked,
+                                                                    }
+                                                                )
+                                                            }
+                                                        >
+                                                            <span className="flex items-center">
+                                                                <span className="flex flex-col text-sm">
+                                                                    <RadioGroup.Label
+                                                                        className="font-medium text-gray-900"
+                                                                        as="span"
+                                                                    >
+                                                                        {
+                                                                            option.label
+                                                                        }
+                                                                    </RadioGroup.Label>
+
+                                                                    {option.description ? (
+                                                                        <RadioGroup.Description
+                                                                            as="span"
+                                                                            className="text-gray-500"
+                                                                        >
+                                                                            <span className="block sm:inline">
+                                                                                {
+                                                                                    option.description
+                                                                                }
+                                                                            </span>
+                                                                        </RadioGroup.Description>
+                                                                    ) : null}
+                                                                </span>
+                                                            </span>
+
+                                                            <RadioGroup.Description
+                                                                as="span"
+                                                                className="mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right"
+                                                            >
+                                                                <span className="font-medium text-gray-900">
+                                                                    {formatPrice(
+                                                                        option.price /
+                                                                            100
+                                                                    )}
+                                                                </span>
+                                                            </RadioGroup.Description>
+                                                        </RadioGroup.Option>
+                                                    )
+                                                )}
+                                            </div>
+                                        </RadioGroup>
+                                    )
+                                )}
                             </div>
                         </div>
                     </div>
                 </ScrollArea>
+
+                <div className="w-full px-8 h-16 bg-white">
+                    <div className="h-px w-full bg-zinc-200" />
+                    <div className="w-full h-full flex justify-end items-center">
+                        <div className="w-full flex gap-6 items-center">
+                            <p className="font-medium whitespace-nowrap">
+                                {formatPrice(
+                                    (BASE_PRICE +
+                                        options.finish.price +
+                                        options.material.price) /
+                                        100
+                                )}
+                            </p>
+                            <Button
+                                size="sm"
+                                className="w-full"
+                            >
+                                Continue
+                                <ArrowRight className="h-4 w-4 ml-1.5 inline" />
+                            </Button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
