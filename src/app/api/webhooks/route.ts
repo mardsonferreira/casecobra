32c4de6e9d4a6396d36be95030e9ac1a634324d9
mardsonferreira <mardsonferreira25@gmail.com>
2024-09-26 23:31:37 -0300

    [Ferreira] Finish project

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -9,6 +9,8 @@ import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const adminEmail = process.env.ADMIN_EMAIL;
 
+console.log("ADMIN EMAIL", adminEmail);
+
 export async function POST(req: Request) {
     try {
         const body = await req.text();
